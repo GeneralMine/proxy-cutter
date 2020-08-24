197d149ba4cbaf8ce730f521dfe05da33dd5704d
Marvin Raiser <marvin_raiser@web.de>
2020-08-24 13:35:58 +0200

    Fix missing slash error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
     req.url = req.url.slice(1, req.url.length);
     if(req.url.indexOf("/") == -1 ){
-        req.url = "/" + req.url;
+        req.url = "/";
     } else {
         req.url = req.url.slice(req.url.indexOf("/"), req.url.length);
     }
